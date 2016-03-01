44d4adf3a9cdcd65a242f53bbe4a9fee872e6df5
Abraham Kuri <kurenn@icalialabs.com>
2016-02-29 19:31:46 -0600

    Adds AWS support for amazon

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  Carrierwave.configure do |config|
+    config.storage :file
+  end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'                        # required
+    config.fog_credentials = {
+      provider:              'AWS',                        # required
+      aws_access_key_id:     'AKIAJSCEB45XDXPDW3KA',                        # required
+      aws_secret_access_key: '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'                        # require
+    }
+    config.fog_directory  = 'itesm-blog'                          # required
+    config.fog_public     = false                                        # optional, defaults to true
+    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+  end
 end
