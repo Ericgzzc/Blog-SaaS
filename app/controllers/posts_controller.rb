65c06cf95aa8a280d66dc95a4bf2c178178b6d52
Abraham Kuri <kurenn@icalialabs.com>
2016-02-29 18:03:14 -0600

    Adds authentication & authorization for posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user!, only: [:destroy, :edit, :update]
+
   def index
     @posts = Post.recent
   end
@@ -8,11 +11,9 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params) # => {title: '', content: ''}
       redirect_to "/posts/#{@post.id}/edit"
     else
@@ -21,11 +22,11 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
     if @post.save
       redirect_to "/posts/#{@post.id}"
     else
@@ -34,12 +35,19 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    post = Post.find(params[:id])
-    post.destroy
+    @post.destroy
     redirect_to "/posts"
   end
 
   private
+
+    def correct_user!
+      @post = current_user.posts.find_by(id: params[:id])
+      redirect_to "/posts" unless @post
+
+      #@post = Post.find(params[:id])
+      #redirect_to posts_path if @post.user_id != current_user.id
+    end
 
     def post_params
       params.require(:post).permit(:title, :content)
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,6 @@
 module PostsHelper
+
+  def can_destroy_post?(post)
+    user_signed_in? && post.user_id == current_user.id
+  end
 end
